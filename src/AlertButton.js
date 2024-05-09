9ee5a896f006a22848333011306ad3c76619956b
Etsegenet <bw.etsegenetalamerew@gmail.com>
2024-05-08 22:19:35 -0700

    update

---
+++ src/AlertButton.js
@@ -0,0 +1,18 @@
+// src/AlertButton.js
+import React from 'react';
+
+const AlertButton = ({ message, children }) => {
+  const handleClick = () => {
+    alert(message);
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
+
+  
+
+export default AlertButton;
